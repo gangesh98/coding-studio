7d507221adf64c7e14dfbd623fd0dd1cc54ac35a
OpenSense Labs MBP - 1 <osl_mbp-1@OpenSenses-MacBook-Pro.local>
2019-06-17 19:56:39 +0530

    june-17 modification

---
+++ js/codingstudio.js
@@ -0,0 +1,12 @@
+(Content-Type: text/js)
+ <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+<span onclick="openNav()">&#9776; </span>
+
+	<script>
+	function openNav() {
+	  document.getElementById("mySidenav").style.width = "100%";
+	}
+	function closeNav() {
+	  document.getElementById("mySidenav").style.width = "0";
+	}
+	</script>
