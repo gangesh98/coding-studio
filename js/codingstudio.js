5fcf041293479d30d19cad63368a9c383def6c00
OpenSense Labs MBP - 1 <osl_mbp-1@OpenSenses-MacBook-Pro.local>
2019-06-20 19:47:01 +0530

    20-june

--- js/codingstudio.js
+++ js/codingstudio.js
@@ -8,20 +8,22 @@
 	}
 	// slider-main
 	$(document).ready(function(){
-  
- $('.main-cont').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  asNavFor: '.nav-row'
-});
-$('.nav-row').slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  asNavFor: '.main-cont',
-  dots: true,
-  centerMode: true,
-  focusOnSelect: true
-});
-});
+	
+	 $('.main-cont').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  fade: true,
+	  asNavFor: '.nav-row'
+	});
+	$('.nav-row').slick({
+	  slidesToShow: 5,
+	  slidesToScroll: 1,
+	  asNavFor: '.main-cont',
+	  dots: false,
+	  arrows: false,
+	  centerMode: true,
+	  focusOnSelect: true,
+	  // $('.slick-prev').hide();
+   //    $('.slick-next').hide();
+   	});
+	});
